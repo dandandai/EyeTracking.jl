8d2acd183565ce6c574396fef3515c26c7a9e971
Anna <anna.dai@student.adelaide.edu.au>
2019-04-26 12:25:39 +0930

    New tests

--- test/jparser.jl
+++ test/jparser.jl
@@ -7,10 +7,18 @@ using CSV
     @test output == "Output.csv"
 
     df = CSV.read(output)
-    @test df[3,:GazeDirectionLY] == 0.4455
+
+    #Test whether the data at the same timestamp has been merged into one row
+    @test df[2,:Timestamp] == 191520960
+    #Test whether the array data has been converted to separate columns
     @test df[10,:AccelerometerX] == -1.138
-    @test df[22,:GyroscopeX] == -10.71
+    @test df[22,:GyroscopeX] == -4.486
+    #Test whether the GazeDirection data is distinguished from the left and right eyes
+    @test df[3,:GazeDirectionLY] == 0.4455
+    @test df[39,:GazeDirectionRZ] == 0.8849
+    #Test whether the PipelineVer as a int type can be displayed in csv
     @test df[40,:PipelineVer] == 1502174499
+    #Test whether the SignalDir as a string type can be displayed in csv
     @test df[41,:SignalDir] == "out"
 
 end
